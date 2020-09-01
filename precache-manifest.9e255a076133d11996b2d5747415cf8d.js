075b84b548b3f2df0834ebe3b7c4ffc965288d44
yuri014 <yuriflores2012@gmail.com>
2020-09-01 01:50:47 -0300

    Updates

---
+++ precache-manifest.9e255a076133d11996b2d5747415cf8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270e7df348d3afa6402eb68cd583d7a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94a925c867c9494c28b7",
+    "url": "./static/css/main.38dfc845.chunk.css"
+  },
+  {
+    "revision": "978514e8e5c7eba029e9",
+    "url": "./static/js/2.348fad7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.348fad7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a925c867c9494c28b7",
+    "url": "./static/js/main.462b4023.chunk.js"
+  },
+  {
+    "revision": "423dd4bf3e2253016c7d",
+    "url": "./static/js/runtime-main.d0d49eb2.js"
+  },
+  {
+    "revision": "539048881444e6eb758ab910856b5f4a",
+    "url": "./static/media/background.53904888.jpg"
+  }
+]);
